0f1b1605beeda03f431197b6df579a675b70dfd2
netawater <netawater@25d747dc-0ff4-11de-99c7-9d618077940f>
2009-03-23 13:30:41 +0000

    Use xhtml1.1 instead html with markup strings. It is good based on my testing.

--- blogger.el
+++ blogger.el
@@ -1,24 +1,29 @@
-;; Blogger 用のスタイル
-(setq muse-blogger-markup-strings
-      (copy-tree muse-html-markup-strings))
+;; ;; Blogger 用のスタイル
+;; (setq muse-blogger-markup-strings
+;;       (copy-tree muse-html-markup-strings))
 
-(mapcar #'(lambda (arg)
-            (rplacd (assoc (car arg) muse-blogger-markup-strings)
-                    (cdr arg)))
-        '((section           . "<h3>")
-          (section-end       . "</h3>")
-          (subsection        . "<h4>")
-          (subsection-end    . "</h4>")
-          (subsubsection     . "<h5>")
-          (subsubsection-end . "</h5>")
-          (section-other     . "<h6>")
-          (section-other-end . "</h6>")
-          (begin-example     . "<pre class='src'>")))
+;; (mapcar #'(lambda (arg)
+;;             (rplacd (assoc (car arg) muse-blogger-markup-strings)
+;;                     (cdr arg)))
+;;         '((section           . "<h3>")
+;;           (section-end       . "</h3>")
+;;           (subsection        . "<h4>")
+;;           (subsection-end    . "</h4>")
+;;           (subsubsection     . "<h5>")
+;;           (subsubsection-end . "</h5>")
+;;           (section-other     . "<h6>")
+;;           (section-other-end . "</h6>")
+;;           (begin-example     . "<pre class='src'>")))
+
 (muse-derive-style
- "blogger" "html"
+ ;; Plato Wu,2009/03/05: use xhtml1.1 to avoid the problem of
+ ;; symmetry requirement by blogger
+ "blogger" "xhtml1.1"
  :header  ""
  :footer  ""
- :strings 'muse-blogger-markup-strings
+ ;; Plato Wu,2009/03/05: it seems not need markup-strings after using
+ ;; xhtml1.1
+;; :strings 'muse-blogger-markup-strings
  )
 
 (defun blogger-post ()

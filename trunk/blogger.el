6a6a3a567538639ba1efd0dcf997e97c1c5fe805
read.eval.print <read.eval.print@25d747dc-0ff4-11de-99c7-9d618077940f>
2009-03-28 01:40:27 +0000

    add defvar muse-blogger-markup-strings for for various styles. I'd like markup section to h4, subsection to h5..., and example to <pre style='src'>.

--- trunk/blogger.el
+++ trunk/blogger.el
@@ -1,30 +1,21 @@
-;; ;; Blogger 用のスタイル
-;; (setq muse-blogger-markup-strings
-;;       (copy-tree muse-html-markup-strings))
-
-;; (mapcar #'(lambda (arg)
-;;             (rplacd (assoc (car arg) muse-blogger-markup-strings)
-;;                     (cdr arg)))
-;;         '((section           . "<h3>")
-;;           (section-end       . "</h3>")
-;;           (subsection        . "<h4>")
-;;           (subsection-end    . "</h4>")
-;;           (subsubsection     . "<h5>")
-;;           (subsubsection-end . "</h5>")
-;;           (section-other     . "<h6>")
-;;           (section-other-end . "</h6>")
-;;           (begin-example     . "<pre class='src'>")))
+(defvar muse-blogger-markup-strings '()
+  "Strings used for marking up text as XHMTL for Blogger.
+Example:
+((section . \"<h4>\")
+ (section-end . \"</h4>\")
+ (subsection . \"<h5>\")
+ (subsection-end . \"</h5>\")
+ (subsubsection . \"<h6>\")
+ (subsubsection-end . \"</h6>\")
+ (section-other . \"<div>\")
+ (section-other-end . \"</div>\")
+ (begin-example . \"<pre class='src'>\"))")
 
 (muse-derive-style
- ;; Plato Wu,2009/03/05: use xhtml1.1 to avoid the problem of
- ;; symmetry requirement by blogger
  "blogger" "xhtml1.1"
  :header  ""
  :footer  ""
- ;; Plato Wu,2009/03/05: it seems not need markup-strings after using
- ;; xhtml1.1
-;; :strings 'muse-blogger-markup-strings
- )
+ :strings 'muse-blogger-markup-strings)
 
 (defun blogger-post ()
   (interactive)

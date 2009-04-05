265e3b8b04d9abd318507586edd527fe993fd150
netawater <netawater@25d747dc-0ff4-11de-99c7-9d618077940f>
2009-04-05 14:36:35 +0000

    2009-04-05  Plato Wu  <standin-000@tianya.cn>
    	* blogger.el (muse-html-markup-footnote): 
    	redefine it to support multi post's footnote.

--- trunk/blogger.el
+++ trunk/blogger.el
@@ -24,3 +24,37 @@ Example:
   (slime-repl-send-string
    (format "(progn (require :blogger) (funcall (read-from-string \"blogger:post\") #p\"%s\"))"
            (buffer-file-name))))
+
+(defun muse-html-markup-footnote ()
+  (cond
+   ((get-text-property (match-beginning 0) 'muse-link)
+    nil)
+   ((= (muse-line-beginning-position) (match-beginning 0))
+    (prog1
+        (let ((text (match-string 1)))
+          (muse-insert-markup
+           (concat "<p class=\"footnote\">"
+                   "<a class=\"footnum\" name=\"" (muse-publishing-directive "title") "fn." text
+;                   "\" href=\"#fnr." text "\">"
+		   "\" href=\"#" (muse-publishing-directive "title") "fnr." text "\">"
+                   text ".</a>")))
+      (save-excursion
+        (save-match-data
+          (let* ((beg (goto-char (match-end 0)))
+                 (end (and (search-forward "\n\n" nil t)
+                           (prog1
+                               (copy-marker (match-beginning 0))
+                             (goto-char beg)))))
+            (while (re-search-forward (concat "^["
+                                              muse-regexp-blank
+                                              "]+\\([^\n]\\)")
+                                      end t)
+              (replace-match "\\1" t)))))
+      (replace-match "")))
+   (t (let ((text (match-string 1)))
+        (muse-insert-markup
+         (concat "<sup><a class=\"footref\" name=\"" (muse-publishing-directive "title") "fnr." text
+;                 "\" href=\"#fn." text "\">"
+                 "\" href=\"#" (muse-publishing-directive "title") "fn." text "\">"
+                 text "</a></sup>")))
+      (replace-match ""))))
